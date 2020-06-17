1fded0c71da4cfb550ecc75478832dd32c0acdeb
eudeleal <eudeatitude@gmail.com>
2020-06-17 13:20:19 -0300

    atualização prof

--- database/script.sql
+++ database/script.sql
@@ -1,19 +1,16 @@
 CREATE DATABASE concessionaria DEFAULT CHARACTER SET utf8;
 USE concessionaria;
 
-CREATE TABLE Motores
-(
-    MotoresId INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    potencia VARCHAR (3),
-    qntCilindradas INTEGER
+CREATE TABLE Motores (
+    MotorID INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    potencia VARCHAR(3),
+    qntCilindros INTEGER,
+    chassi INTEGER,
+    FOREIGN KEY (chassi) REFERENCES Carros(chassi)
 );
-
-CREATE TABLE carros
-(
+CREATE TABLE Carros (
     chassi INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     marca VARCHAR (255),
     modelo VARCHAR (255),
-    cor VARCHAR (255),
-    MotorId INTEGER NOT NULL,
-    FOREIGN KEY (MotorId) REFERENCES Motores(MotorId)
+    cor VARCHAR (255)
 );
